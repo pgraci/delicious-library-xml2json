4a55d7ac5d0ed41ba78cd3c56f139cdf46de9d70
pgraci <philip@madeinhaus.com>
2017-12-29 13:54:29 -0800

    Remove process blocks since this will just be run from terminal

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
 const parser = require('xml-parser');
-const fs = require('fs')
+const fs = require('fs');
 
-const library = []
-const whitelistedKeys = [ 'uuidString', 'type', 'title', 'coverImageLargeURLString' ]
+const library = [];
+const whitelistedKeys = [
+    'uuidString',
+    'type',
+    'title',
+    'coverImageLargeURLString',
+];
 
-let data = ''
+const xml = fs.readFileSync('library.xml', 'utf8');
 
-process.stdin.setEncoding('utf8');
+if (!xml) {
+    console.log('No library.xml found, exiting...');
+    process.exit();
+}
 
-process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
-  if (chunk !== null) data += chunk
-});
+const data = parser(xml);
+let dictionaries = data.root.children[0].children;
 
-process.stdin.on('end', () => {
-    if (!data) {
-        console.log('No input, exiting...')
-        process.exit()
-    }
+dictionaries.forEach(dictionary => {
+    let currentProduct = {};
+    let currentKey;
 
-    data = parser(data)
-    let dictionaries = data.root.children[0].children
+    dictionary.children.forEach(data => {
+        if (data.name === 'key' && whitelistedKeys.indexOf(data.content) !== -1)
+            currentKey = data.content;
+        else if (currentKey && data.content) {
+            switch (data.name) {
+                case 'integer':
+                    data.content = parseInt(data.content, 10);
+                    break;
 
-    dictionaries.forEach((dictionary) => {
-        let currentProduct = {}
-        let currentKey
+                case 'real':
+                    data.content = parseFloat(data.content);
+                    break;
 
-        dictionary.children.forEach((data) => {
-            if (data.name === 'key' && whitelistedKeys.indexOf(data.content) !== -1) currentKey = data.content
-            else if (currentKey && data.content) {
-                switch (data.name) {
-                    case 'integer':
-                        data.content = parseInt(data.content, 10)
-                        break
-
-                    case 'real':
-                        data.content = parseFloat(data.content)
-                        break
-
-                    case 'date':
-                        data.content = new Date(data.content)
-                        break
-                }
-
-                if (data.content) currentProduct[currentKey] = data.content
-
+                case 'date':
+                    data.content = new Date(data.content);
+                    break;
             }
 
-            if (data.name !== 'key') currentKey = null
-        })
+            if (data.content) currentProduct[currentKey] = data.content;
+        }
 
-        library.push(currentProduct)
-    })
+        if (data.name !== 'key') currentKey = null;
+    });
 
-    library.sort((productA, productB) => productA.title.toLowerCase() > productB.title.toLowerCase() ? 1 : -1)
+    library.push(currentProduct);
+});
 
-    fs.writeFile('./library.json', JSON.stringify(library), (err) => {
-        if (err) console.err(err)
-        else console.log('library.json file written with ' + library.length + ' products')
-    })
+library.sort(
+    (productA, productB) =>
+        productA.title.toLowerCase() > productB.title.toLowerCase() ? 1 : -1
+);
+
+fs.writeFile('./library.json', JSON.stringify(library), err => {
+    if (err) console.err(err);
+    else
+        console.log(
+            'library.json file written with ' + library.length + ' products'
+        );
 });
